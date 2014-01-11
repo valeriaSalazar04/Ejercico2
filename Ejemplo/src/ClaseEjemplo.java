4fd7822aa463a2ffe47d3515477e5e34eee6ee40
ValeriaG <ValeriaG@ValeriaG-PC>
2014-01-11 17:38:41 -0500

    Bueno, acá yo creé el método para hacer un hola mundo.

---
+++ Ejemplo/src/ClaseEjemplo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ValeriaG
+ */
+public class ClaseEjemplo {
+    
+    public static void main (String ar[]) {
+        System.out.println("Hola mundo");
+    }
+    
+    
+    
+}

30e1e69e4fde0806b2fd88a2afe877f4f01ede79
ValeriaG <ValeriaG@ValeriaG-PC>
2014-01-13 19:15:49 -0500

    ejemplo de clases y socializacion...

--- Ejemplo/src/ClaseEjemplo.java
+++ Ejemplo/src/ClaseEjemplo.java
@@ -1,3 +1,6 @@
+
+import java.util.Comparator;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,12 +10,31 @@
  *
  * @author ValeriaG
  */
-public class ClaseEjemplo {
+public class ClaseEjemplo implements Comparable<Object>, Comparator<Object> {
+
+//    
+//    pupblic static void main (String ar[]) {
+//        System.out.println("Hola mundo");
+//             Persona valeria = new Persona(13,"valeria");
+//   
+//        System.out.println(valeria);
+//        Object vector []= new Object [3];
+//        vector [0]= valeria;
+//        System.out.println(valeria);
+//        System.out.println(vector[0].getClass());
+//        valeria=null;
+//        System.out.println(valeria);
+//    }  
     
-    public static void main (String ar[]) {
-        System.out.println("Hola mundo");
+
+    @Override
+    public int compareTo(Object o) {
+     
+        return 0;
     }
-    
-    
-    
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
---
+++ Ejemplo/src/Persona.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ValeriaG
+ */
+public class Persona {
+    
+    
+    
+    private int edad ;
+    private String nombre ;
+    
+    public Persona(){
+        
+        
+        
+    
+    }
+    
+    public Persona (int edad , String nombre){
+     
+        
+    
+    
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+   
+    }
